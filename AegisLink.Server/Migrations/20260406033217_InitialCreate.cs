97cf4638f228a1797ec7b6b8e95ca05263cef6af
ASA WOOTTON <D3338667@live.tees.ac.uk>
2026-04-06 18:44:20 +0100

    Fix NavLink for connection button Create logic to generate ID and Keys Create database

---
+++ AegisLink.Server/Migrations/20260406033217_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AegisLink.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserKeys",
+                columns: table => new
+                {
+                    AegisId = table.Column<string>(type: "TEXT", maxLength: 8, nullable: false),
+                    PublicKey = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserKeys", x => x.AegisId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserKeys_PublicKey",
+                table: "UserKeys",
+                column: "PublicKey",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserKeys");
+        }
+    }
+}
