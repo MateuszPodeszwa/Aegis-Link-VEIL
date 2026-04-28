cd4aae1a9bdc6ba0d63326cf5c3b0da529b3db0e
maxhurst6 <e4499497@live.tees.ac.uk>
2026-04-28 21:30:50 +0100

    auto deleting messages

--- Aegis-Link-VEIL/Models/ChatMessage.cs
+++ Aegis-Link-VEIL/Models/ChatMessage.cs
@@ -4,5 +4,12 @@
     public MessageSender From { get; set; } = MessageSender.System;
     public string Content { get; set; } = string.Empty;
     public bool IsEdited { get; set; } = false;
-    public DateTime SendAt { get; set; } = DateTime.UtcNow;
+    public DateTime SendAt { get; set; }
+    public DateTime DeleteAt { get; set; }
+
+    public ChatMessage()
+    {
+        Id = Guid.NewGuid();
+        SendAt = DateTime.UtcNow;
+    }
 }
